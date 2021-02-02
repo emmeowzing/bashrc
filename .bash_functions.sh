5faccdddb795c295ccfb3db2282b3c83b79d9496
bjd2385 <bjd2385@aperiodicity.com>
2021-02-02 15:57:22 -0500

    Added function to clear all docker images and containers

--- .bash_functions.sh
+++ .bash_functions.sh
@@ -332,3 +332,21 @@ vv() {
 
     return 0
 }
+
+
+##
+# Clear all containers and unused images. Useful after you've just tested a Dockerfile build a bunch of times.
+clear_containers()
+{
+    # Clear containers.
+    for c in $(docker ps -a | awk '{ print $1 }' | tail -n +2)
+    do 
+        docker rm "$c"
+    done
+
+    # Clear images.
+    for im in $(docker images | awk '{ print $3 }' | tail -n +2)
+    do 
+        docker rmi "$im"
+    done
+}
