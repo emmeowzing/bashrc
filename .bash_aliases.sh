86ae48423b2f6249c2c2f94759893fd1d8af70bb
Brandon Doyle <brandon.doyle@hqo.co>
2021-09-16 14:56:32 -0400

    Update git status alias to include diff

--- .bash_aliases.sh
+++ .bash_aliases.sh
@@ -21,9 +21,10 @@ alias tf=terraform
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history | tail -n1 | sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # Git
+alias g='git'
 alias gi='git init .'
 alias ga='git add .'
-alias gs='git status'
+alias gs='git status && git diff'
 alias gb='git branch'
 alias gc='git commit'
 alias gcm='git checkout master'
